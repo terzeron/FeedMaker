fa6d1e3269b82ba56cb0bf88c2f83629d28fffc8
Young-il Cho <terzeron@gmail.com>
2014-02-09 00:29:59 +0900

    Modify the logic of case that exceeds the height limit of PIL

--- bin/download_merge_split.pl
+++ bin/download_merge_split.pl
@@ -14,10 +14,20 @@ sub get_cache_url
 {
 	my $img_url = shift;
 	my $img_ext = shift;
+	my $postfix = shift;
+	my $index = shift;
 	my $url_prefix = "http://terzeron.net/xml/img";
 
+	my $postfix_str = "";
+	if (defined $postfix and $postfix ne "") {
+		$postfix_str = "_" . $postfix;
+	}
+	my $index_str = "";
+	if (defined $index and $index ne "") {
+		$index_str = "." . $index;
+	}
 	if ($img_url =~ m!^http://! and defined $img_ext) {
-		return $url_prefix . "/" . get_md5_name($img_url) . "." . $img_ext;
+		return $url_prefix . "/" . get_md5_name($img_url) . $postfix_str . $index_str . "." . $img_ext;
 	}
 	return $url_prefix . "/" . $img_url;
 }
@@ -27,10 +37,20 @@ sub get_cache_file_name
 {
 	my $img_url = shift;
 	my $img_ext = shift;
+	my $postfix = shift;
+	my $index = shift;
 	my $path_prefix = "/Users/terzeron/public_html/xml/img";
 
+	my $postfix_str = ""; 
+    if (defined $postfix and $postfix ne "") {
+        $postfix_str = "_" . $postfix;
+    }
+    my $index_str = "";
+    if (defined $index and $index ne "") {
+        $index_str = "." . $index;
+    }                     
 	if ($img_url =~ m!^http://! and defined $img_ext) {
-		return $path_prefix . "/" . get_md5_name($img_url) . "." . $img_ext;
+		return $path_prefix . "/" . get_md5_name($img_url) . $postfix_str . $index_str . "." . $img_ext;
 	} 
 	return $path_prefix . "/" . $img_url;
 }
@@ -76,7 +96,7 @@ sub main
 	our $opt_n;
 	getopts($opt_str, \%opts);
 	if (defined $opts{"c"}) {
-		if ($opts{"c"} =~ m!^(white|black)$!) {
+		if ($opts{"c"} =~ m!^(white|black|blackorwhite)$!) {
 			$bgcolor_option = "-c " . $opts{"c"};
 		}
 	}
@@ -125,15 +145,27 @@ sub main
 			$total_height += $height;
 		}
 		#print "total_height=$total_height\n";
+		my @img_file_arr1 = ();
+		my @img_file_arr2 = ();
 		if ($total_height > 65500) {
-			# no merge mode
-			for my $img_url (@img_url_arr) {
-				print_local_path($img_url, $img_ext);
-			}
-		} else {
+			# split array into two sub-array
+			my $half = int((scalar @img_file_arr) / 2); 
+			#print "lengt=" . (scalar @img_file_arr) . " half=$half\n";
+			@img_file_arr1 = splice @img_file_arr, 0, $half;
+			@img_file_arr2 = splice @img_file_arr, 0;
+			#print scalar @img_file_arr1 . "\n";
+			#print scalar @img_file_arr2 . "\n";
+		}  else {
+			@img_file_arr1 = @img_file_arr;
+			@img_file_arr2 = ();
+		}
+
+		my $num = 1;
+		foreach my $img_file_arr_ref (\@img_file_arr1, \@img_file_arr2) {
+			my @img_file_arr = @$img_file_arr_ref;
 			# merge mode
-			my $merged_img_file = get_cache_file_name($url, $img_ext);
-			$cmd = qq(../../../CartoonSplit/merge.py ${merged_img_file} );
+			my $merged_img_file = get_cache_file_name($url, $img_ext, $num);
+			$cmd  = qq(../../../CartoonSplit/merge.py ${merged_img_file} );
 			for my $cache_file (@img_file_arr) {
 				$cmd .= $cache_file . " ";
 			}
@@ -172,14 +204,15 @@ sub main
 				
 			# print the split images
 			for (my $i = 1; $i <= $num_units; $i++) {
-				my $split_img_file = get_cache_file_name(get_md5_name($url) . "." . $i . "." . $img_ext);
+				my $split_img_file = get_cache_file_name($url, $img_ext, $num, $i);
 				if (-e $split_img_file) {
-					my $split_img_url = get_cache_url(get_md5_name($url) . "." . $i . "." . $img_ext);
+					my $split_img_url = get_cache_url($url, $img_ext, $num, $i);
 					print "<img src='${split_img_url}' width='100%'/>\n";
 				} else {
 					last;
 				}
 			}
+			$num++;
 		}
 	}	
 }
