5af698eb7ecbf30f3b75c5ab890c2a8304b7c12b
Young-il Cho <terzeron@gmail.com>
2014-02-17 15:22:58 +0900

    some trivial fixes in comments

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,15 +9,15 @@ else
 	path=${PWD}/
 fi
 export FEED_MAKER_HOME=$(echo $path | gsed -r 's/((fm|[Ff]eed[Mm]aker)[^\/]*).*/\1/')
-#echo FEED_MAKER_HOME=${FEED_MAKER_HOME}
+echo FEED_MAKER_HOME=${FEED_MAKER_HOME}
 
 BIN_DIR=${FEED_MAKER_HOME}/bin
 export PATH=~/bin:.:${BIN_DIR}:/usr/local/bin/:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:${PATH}
 
-# perl path
+# python path
 export PYTHONPATH=${BIN_DIR}:/usr/local/lib/python2.7/site-packages:${PYTHONPATH}
 
-# python path
+# perl path
 export PERLBREW_ROOT=/Users/terzeron/perl5/perlbrew
 . ${PERLBREW_ROOT}/etc/bashrc
 PERL_INSTALLED_VERSION=5.19.5
