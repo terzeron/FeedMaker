2efaa2703ca05e0f8f135ec45ab8c3878ab07672
Young-il Cho <terzeron@gmail.com>
2014-03-31 18:28:54 +0900

    Add some new configurations and modify the minor changes

--- tstore/post_process_tstore.pl
+++ tstore/post_process_tstore.pl
@@ -13,35 +13,18 @@ sub main
 	my $img_url_prefix = "";
 	my $img_index = "";
 	my $img_ext = "";
-	my $state = 0;
 
 	while (my $line = <STDIN>) {
-		if ($state == 0) {
-			if ($line =~ m!<img src='(http://m.tstore.co.kr/SMILE_DATA[^']+)(\d+)\.(jpg)'/>!) {
-				$img_url_prefix = $1;
-				$img_index = int($2);
-				$img_ext = $3;
-				$state = 1;
-			}
-		} elsif ($state == 1) {
-			# 시작 인덱스
-			if ($line =~ m!^\s*1</span>\s*$!) {
-				$state = 2;
-			}
-		} elsif ($state == 2) {
-			if ($line =~ m!^\s*/<span>\s*$!) {
-				$state = 3;
-			}
-		} elsif ($state == 3) {
-			# 종료 인덱스
-			if ($line =~ m!^\s*(\d+)</span>\s*$!) {
-				my $img_last_index = int($1);
-				for (my $i = 1; $i <= $img_last_index; $i++) {
-					print "<img src='$img_url_prefix$i.$img_ext' width='100%'/>\n";
-				}
-				last;
-			}
+		if ($line =~ m!<img src='(http://m.tstore.co.kr/SMILE_DATA[^']+)(\d+)\.(jpg)'/>!) {
+			$img_url_prefix = $1;
+			$img_index = int($2);
+			$img_ext = $3;
 		}
+	}
+
+	for (my $i = $img_index; $i <= 20; $i++) {
+		my $url = "$img_url_prefix$i.$img_ext";
+		print "<img src='$url' width='100%'/>\n";
 	}
 }
 
