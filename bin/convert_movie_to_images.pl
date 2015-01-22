770a67c5da1b85c652f72c0b3e8e0d584a9511e4
Young-il Cho <terzeron@gmail.com>
2015-01-22 09:50:10 +0900

    1) add some common tools

---
+++ bin/convert_movie_to_images.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+
+use English;
+use strict;
+use warnings;
+use Carp;
+use Modern::Perl;
+use Digest::MD5;
+use FeedMaker qw(get_md5_name);
+use Getopt::Std;
+use File::Path;
+use Cwd;
+
+
+sub main
+{
+	my $link = $ARGV[0];
+	my $id = get_md5_name($link);
+	my $video_file = $id . ".avi";
+	my $img_dir = "/Users/terzeron/public_html/xml/img/thegoodmovie";
+	my $img_url_prefix = "http://terzeron.net/xml/img/thegoodmovie";
+	my $cmd;
+	my $result;
+
+	while (my $line = <STDIN>) {
+		if ($line =~ m!<video src='[^']*videoPath=(rtmp://[^&]*)[^']*'>!) {
+			my $movie_url = $1;
+			if (not -e "${img_dir}/${id}_0001.jpg") { 
+				if (not -e $video_file) {
+					$cmd = qq(/Users/terzeron/workspace/rtmpdump/rtmpdump -q -r '$movie_url' > ${video_file});
+					print "<!-- $cmd -->\n";
+					$result = qx($cmd);
+					print "<!-- $result -->";
+				}
+				
+				$cmd = qq(/Users/terzeron/bin/extract_images_from_video.sh ${video_file} "${img_dir}/${id}_" > /dev/null 2>&1);
+				print "<!-- $cmd -->\n";
+				$result = qx($cmd);
+				print "<!-- $result -->";
+			}
+
+			opendir(my $dh, $img_dir);
+			while (my $file = readdir($dh)) {
+				if ($file =~ m!${id}_\d+\.jpg$!x) {
+					print "<img src='${img_url_prefix}/$file'/>\n";
+				}
+			}
+			closedir($dh);
+
+			#unlink($video_file);
+		}
+	}
+}
+
+
+main();
---
+++ bin/download_image.pl
@@ -0,0 +1,120 @@
+#!/usr/bin/env perl
+
+use English;
+use strict;
+use warnings;
+use Carp;
+use Modern::Perl;
+use Digest::MD5;
+use FeedMaker qw(get_md5_name);
+use Getopt::Std;
+use File::Path;
+use Cwd;
+
+
+sub get_cache_url
+{
+	my $url_prefix = shift;
+	my $img_url = shift;
+	my $img_ext = shift;
+	my $postfix = shift;
+	my $index = shift;
+
+	my $postfix_str = "";
+	if (defined $postfix and $postfix ne "") {
+		$postfix_str = "_" . $postfix;
+	}
+	my $index_str = "";
+	if (defined $index and $index ne "") {
+		$index_str = "." . $index;
+	}
+	if ($img_url =~ m!^http://! and defined $img_ext) {
+		return $url_prefix . "/" . get_md5_name($img_url) . $postfix_str . $index_str . "." . $img_ext;
+	}
+	return $url_prefix . "/" . $img_url;
+}
+
+
+sub get_cache_file_name
+{
+	my $path_prefix = shift;
+	my $img_url = shift;
+	my $img_ext = shift;
+	my $postfix = shift;
+	my $index = shift;
+
+	my $postfix_str = ""; 
+    if (defined $postfix and $postfix ne "") {
+        $postfix_str = "_" . $postfix;
+    }
+    my $index_str = "";
+    if (defined $index and $index ne "") {
+        $index_str = "." . $index;
+    }                     
+	if ($img_url =~ m!^http://! and defined $img_ext) {
+		return $path_prefix . "/" . get_md5_name($img_url) . $postfix_str . $index_str . "." . $img_ext;
+	} 
+	return $path_prefix . "/" . $img_url;
+}
+
+
+sub download_image
+{
+	my $path_prefix = shift;
+	my $img_url = shift;
+	my $img_ext = shift;
+	my $page_url = shift;
+
+	my $cache_file = get_cache_file_name($path_prefix, $img_url, $img_ext);
+	my $cmd = qq([ -f "$cache_file" -a -s "$cache_file" ] || wget.sh --download "$img_url" "$cache_file" "$page_url");
+	#print $cmd . "\n";
+	my $result = qx($cmd);
+	#print $result;
+	if ($CHILD_ERROR != 0) {
+		return -1;
+	}
+	return 0;
+}
+
+
+sub main
+{
+	my $cwd = getcwd;
+	my $feed_name = qx(basename $cwd);
+	chomp $feed_name;
+	my $img_url_prefix = "http://terzeron.net/xml/img/$feed_name";
+	my $path_prefix = "/Users/terzeron/public_html/xml/img/$feed_name";
+
+	my $img_prefix = "";
+	my $img_index = -1;
+	my $img_ext = "jpg";
+	my $num_units = 25;
+
+	my $url = $ARGV[0];
+	my $cmd = "";
+	my $result = "";
+
+	mkpath($path_prefix);
+
+	while (my $line = <STDIN>) {
+		if ($line =~ m!<img src=(?:["'])([^"']+)(?:["']) width='\d+%?'/?>!) {
+			my $img_url = $1;
+			#print $img_url . "\n";
+			# download
+			if (download_image($path_prefix, $img_url, $img_ext, $url) < 0) {
+				#confess "Error: can't download the image from '$img_url', $ERRNO\n";
+				next;
+			}
+			
+			my $cache_file = get_cache_file_name($path_prefix, $img_url, $img_ext);
+			my $cache_url = get_cache_url($img_url_prefix, $img_url, $img_ext);
+			#print "<!-- $img_url -> $cache_file / $cache_url -->\n";
+			print "<img src='$cache_url'/>\n";
+		} else {
+			print $line . "\n";
+		}
+	}
+}
+
+
+main();
