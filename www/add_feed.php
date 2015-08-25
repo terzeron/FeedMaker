c0c0e6119f8420ea8ab35e13025c98e8adfefdb9
Young-il Cho <terzeron@gmail.com>
2015-08-25 19:59:03 +0900

    modify web server pages

--- www/add_feed.php
+++ www/add_feed.php
@@ -51,306 +51,306 @@ if (is_dir($work_dir)) {
 </div>
 
 <script type="text/javascript">
- var ajax_url = "exec.php";
- 
- function check_feed_name(feed_name) {
-	 if (feed_name == undefined || feed_name == "") {
-		 alert("Feed 이름을 입력하세요." + "feed_name='" + feed_name + "'");
-		 return -1;
-	 }
-	 return 0;
- }
- 
- function get_error_message(str) {
-	 return "<span style='color:red;'>" + str + "</span>";
- }
- 
- function get_success_message(str) {
-	 return "<span style='color:green;'>" + str + "</span>";
- }
- 
- // dir_list selectbox event handler
- $("#dir_list").change(function() {
-	 $("#dir_list option:selected").each(function() {
-		 var parent_name = $(this).val();
-		 $.post(
-			 ajax_url,
-			 { "command": "get_feed_list", "parent_name": parent_name },
-			 function(data, textStatus, jqXHR) {
-				 res = jQuery.parseJSON(data);
-				 if (res["result"] != "0") {
-					 alert("can't get feed list");
-				 } else {
-					 html = "";
-					 for (var i = 0; i < res["message"].length; i++) {
-						 html += '<option value="' + res["message"][i] + '">' + res["message"][i] + '</option>'; 
-					 }
-					 $("#feed_list").html(html);
-				 }
-			 }
-		 );
-	 });
-	 resetHandler();
- });
+var ajax_url = "exec.php";
 
- $("#feed_list").change(function() {
-	 $("#feed_list option:selected").each(function() {
-         var parent_name = $("#dir_list").val();
-         var sample_feed = $(this).val();
-		 $.post(
-			 ajax_url,
-			 { "command": "get_feed_content", "sample_feed": $(this).val(), "parent_name": $("#dir_list").val() },
-			 function(data, textStatus, jqXHR) {
-				 res = jQuery.parseJSON(data);
-				 if (res["result"] != "0") {
-					 alert("can't get feed content");
-				 } else {
-					 html = res["message"];
-					 new_html = html;
-					 new_html = new_html.replace(/</g, "&lt;")
-										.replace(/>/g, "&gt;<br/>")
-									 .replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;")
-									 .replace(/&lt;!\[CDATA\[(.*)\]\]&gt;/g, "&lt;![CDATA[<input class='cdata' type='text' value=\"\$1\" size='80'/>]]&gt;");
-					 $("#xml").html(new_html);
-				 }
-			 }
-		 );
-	 });
-	 resetHandler();
- });
- 
- // save button event handler
- var saveHandler = function() {
-	 $("#save").val("저장 중");
-	 var feed_name = $("#feed_name").val();
-	 var parent_name = $("#dir_list").val();
-	 var sample_feed = $("#feed_list option:selected").val();
-	 if (check_feed_name(feed_name) < 0) {
-		 return -1;
-	 }
-	 var cdata_arr = read_form_cdata();
-	 var xml_text = replace_form_with_cdata(cdata_arr);
-	 $.post(
-		 ajax_url,
-		 { "command": "save", "feed_name": feed_name, "parent_name": parent_name, "sample_feed": sample_feed, "xml_text": xml_text },
-		 function(data, textStatus, jqXHR) {
-			 res = jQuery.parseJSON(data);
-			 if (res["result"] != "0") {
-				 $("#status").html(get_error_message(res["message"]));
-			 } else {
-				 $("#status").html(get_success_message(feed_name + ".xml 파일이 저장되었습니다."));
-				 $("#save").val("저장 완료");
-				 $("#save").button({disabled: true});
-			 }
-		 }
-	 );
- };
- 
- // lint button event handler
- var lintHandler = function() {
-	 $("#lint").val("XML lint 실행 중");
-	 var feed_name = $("#feed_name").val();
-	 var parent_name = $("#dir_list").val();
-	 var sample_feed = $("#feed_list option:selected").val();
-	 $.post(
-		 ajax_url,
-		 { "command": "lint", "feed_name": feed_name, "parent_name": parent_name, "sample_feed": sample_feed },
-		 function(data, textStatus, jqXHR) {
-			 res = jQuery.parseJSON(data);
-			 if (res["result"] != "0") {
-				 $("#status").html(get_error_message(res["message"]));
-			 } else {
-				 $("#status").html(get_success_message("XML 검사 완료"));
-				 $("#lint").val("XML lint 실행 완료");
-				 $("#lint").button({disabled: true});
-			 }
-		 }
-	 );
- };	 
+function check_feed_name(feed_name) {
+	if (feed_name == undefined || feed_name == "") {
+		alert("Feed 이름을 입력하세요." + "feed_name='" + feed_name + "'");
+		return -1;
+	}
+	return 0;
+}
 
- // extract button event handler
- var extractHandler = function() {
-	 $("#extract").val("추출 실행 중");
-	 var feed_name = $("#feed_name").val();
-	 var parent_name = $("#dir_list").val();
-	 var sample_feed = $("#feed_list option:selected").val();
-	 $.post(
-		 ajax_url,
-		 { "command": "extract", "feed_name": feed_name, "parent_name": parent_name, "sample_feed": sample_feed },
-		 function(data, textStatus, jqXHR) {
-			 res = jQuery.parseJSON(data);
-			 if (res["result"] != "0") {
-				 $("#status").html(get_error_message(res["message"]));
-				 $("#extract").val("재추출 시도");
-			 } else {
-				 $("#status").html(get_success_message("피드 추출 성공"));
-				 $("#extract").val("추출 실행 완료");
-				 $("#extract").button({disabled: true});
-			 }
-		 }
-	 );
- };
+function get_error_message(str) {
+	return "<span style='color:red;'>" + str + "</span>";
+}
 
- // setacl button event handler
- var setAclHandler = function() {
-	 $("setacl").val("ACL 설정 중");
-	 var feed_name = $("#feed_name").val();
-	 var parent_name = $("#dir_list").val();
-	 var sample_feed = $("#feed_list option:selected").val();
-	 $.post(
-		 ajax_url,
-		 { "command": "setacl", "feed_name": feed_name, "parent_name": parent_name, "sample_feed": sample_feed },
-		 function(data, textStatus, jqXHR) {
-			 res = jQuery.parseJSON(data);
-			 if (res["result"] != "0") {
-				 $("#status").html(get_error_message(res["message"]));
-			 } else {
-				 $("#status").html(get_success_message("ACL 설정 성공"));
-				 $("#setacl").val("ACL 설정 완료");
-				 $("#setacl").button({disabled: true});
-				 $("#feedly_link").attr('href', 'http://feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Fterzeron.net%2F' + feed_name + '.xml');
-				 $("#feedly_link").css('display', 'block');
-			 }
-		 }
-	 );
- };
+function get_success_message(str) {
+	return "<span style='color:green;'>" + str + "</span>";
+}
 
- // reset button event handler
- var resetHandler = function() {
- 	 $("#save").val("저장");
-	 $("#save").button({disabled: false})
- 	 $("#lint").val("XML lint 실행");
-	 $("#lint").button({disabled: false})
- 	 $("#extract").val("추출 실행");
-	 $("#extract").button({disabled: false})
- 	 $("#setacl").val("ACL 설정");
-	 $("#setacl").button({disabled: false})
- 	 $("#disable").val("비활성화");
-	 $("#disable").button({disabled: false})
- 	 $("#remove").val("삭제");
-	 $("#remove").button({disabled: false})
-	 $("#feedly_link").css('display', 'hidden');
- };
- 
- // remove button event handler
- var removeHandler = function() {
-	 $("remove").val("삭제 중");
-	 //var feed_name = $("#feed_name").val();
-	 var parent_name = $("#dir_list").val();
-	 var sample_feed = $("#feed_list option:selected").val();
-	 $.post(
-		 ajax_url,
-		 { "command": "remove", "parent_name": parent_name, "sample_feed": sample_feed },
-		 function(data, textStatus, jqXHR) {
-			 res = jQuery.parseJSON(data);
-			 if (res["result"] != "0") {
-				 $("#status").html(get_error_message(res["message"]));
-			 } else {
-				 $("#status").html(get_success_message("삭제 성공"));
-				 $("#remove").val("삭제 완료");
-				 $("#remove").button({disabled: true});
-			 }
-		 }
-	 );
- };
+// dir_list selectbox event handler
+$("#dir_list").change(function() {
+	$("#dir_list option:selected").each(function() {
+		var parent_name = $(this).val();
+		$.post(
+			ajax_url,
+			{ "command": "get_feed_list", "parent_name": parent_name },
+			function(data, textStatus, jqXHR) {
+				res = jQuery.parseJSON(data);
+				if (res["result"] != "0") {
+					alert("can't get feed list");
+				} else {
+					html = "";
+					for (var i = 0; i < res["message"].length; i++) {
+						html += '<option value="' + res["message"][i] + '">' + res["message"][i] + '</option>'; 
+					}
+					$("#feed_list").html(html);
+				}
+			}
+		);
+	});
+	resetHandler();
+});
 
- // remove button event handler
- var disableHandler = function() {
-	 $("disable").val("비활성화 중");
-	 //var feed_name = $("#feed_name").val();
-	 var parent_name = $("#dir_list").val();
-	 var sample_feed = $("#feed_list option:selected").val();
-	 $.post(
-		 ajax_url,
-		 { "command": "disable", "parent_name": parent_name, "sample_feed": sample_feed },
-		 function(data, textStatus, jqXHR) {
-			 res = jQuery.parseJSON(data);
-			 if (res["result"] != "0") {
-				 $("#status").html(get_error_message(res["message"]));
-			 } else {
-				 $("#status").html(get_success_message("비활성화 성공"));
-				 $("#disable").val("비활성화 완료");
-				 $("#disable").button({disabled: true});
-			 }
-		 }
-	 );
- };
+$("#feed_list").change(function() {
+	$("#feed_list option:selected").each(function() {
+        var parent_name = $("#dir_list").val();
+        var sample_feed = $(this).val();
+		$.post(
+			ajax_url,
+			{ "command": "get_feed_content", "sample_feed": $(this).val(), "parent_name": $("#dir_list").val() },
+			function(data, textStatus, jqXHR) {
+				res = jQuery.parseJSON(data);
+				if (res["result"] != "0") {
+					alert("can't get feed content");
+				} else {
+					html = res["message"];
+					new_html = html;
+					new_html = new_html.replace(/</g, "&lt;")
+						.replace(/>/g, "&gt;<br/>")
+						.replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;")
+						.replace(/&lt;!\[CDATA\[(.*)\]\]&gt;/g, "&lt;![CDATA[<input class='cdata' type='text' value=\"\$1\" size='80'/>]]&gt;");
+					$("#xml").html(new_html);
+				}
+			}
+		);
+	});
+	resetHandler();
+});
 
- $("#save").button().click(saveHandler);
- $("#lint").button().click(lintHandler);
- $("#extract").button().click(extractHandler);
- $("#setacl").button().click(setAclHandler);
- $("#reset").button().click(resetHandler);
- $("#remove").button().click(removeHandler);
- $("#disable").button().click(disableHandler);
+// save button event handler
+var saveHandler = function() {
+	$("#save").val("저장 중");
+	var feed_name = $("#feed_name").val();
+	var parent_name = $("#dir_list").val();
+	var sample_feed = $("#feed_list option:selected").val();
+	if (check_feed_name(feed_name) < 0) {
+		return -1;
+	}
+	var cdata_arr = read_form_cdata();
+	var xml_text = replace_form_with_cdata(cdata_arr);
+	$.post(
+		ajax_url,
+		{ "command": "save", "feed_name": feed_name, "parent_name": parent_name, "sample_feed": sample_feed, "xml_text": xml_text },
+		function(data, textStatus, jqXHR) {
+			res = jQuery.parseJSON(data);
+			if (res["result"] != "0") {
+				$("#status").html(get_error_message(res["message"]));
+			} else {
+				$("#status").html(get_success_message(feed_name + ".xml 파일이 저장되었습니다."));
+				$("#save").val("저장 완료");
+				$("#save").button({disabled: true});
+			}
+		}
+	);
+};
 
- function read_form_cdata() {
-	 var cdata_arr = new Array();
-	 var cdata = $(".cdata");
-	 for (var i = 0; i < cdata.length; i++) {
-		 cdata_arr[i] = cdata[i].value;
-	 }
-	 return cdata_arr;
- }
+// lint button event handler
+var lintHandler = function() {
+	$("#lint").val("XML lint 실행 중");
+	var feed_name = $("#feed_name").val();
+	var parent_name = $("#dir_list").val();
+	var sample_feed = $("#feed_list option:selected").val();
+	$.post(
+		ajax_url,
+		{ "command": "lint", "feed_name": feed_name, "parent_name": parent_name, "sample_feed": sample_feed },
+		function(data, textStatus, jqXHR) {
+			res = jQuery.parseJSON(data);
+			if (res["result"] != "0") {
+				$("#status").html(get_error_message(res["message"]));
+			} else {
+				$("#status").html(get_success_message("XML 검사 완료"));
+				$("#lint").val("XML lint 실행 완료");
+				$("#lint").button({disabled: true});
+			}
+		}
+	);
+};	 
 
- var StringBuffer = function() {
-	 this.buffer = new Array();
- }
- StringBuffer.prototype.append = function(obj) {
-	 this.buffer.push(obj);
- }
- StringBuffer.prototype.toString = function() {
-	 return this.buffer.join("");
- }
+// extract button event handler
+var extractHandler = function() {
+	$("#extract").val("추출 실행 중");
+	var feed_name = $("#feed_name").val();
+	var parent_name = $("#dir_list").val();
+	var sample_feed = $("#feed_list option:selected").val();
+	$.post(
+		ajax_url,
+		{ "command": "extract", "feed_name": feed_name, "parent_name": parent_name, "sample_feed": sample_feed },
+		function(data, textStatus, jqXHR) {
+			res = jQuery.parseJSON(data);
+			if (res["result"] != "0") {
+				$("#status").html(get_error_message(res["message"]));
+				$("#extract").val("재추출 시도");
+			} else {
+				$("#status").html(get_success_message("피드 추출 성공"));
+				$("#extract").val("추출 실행 완료");
+				$("#extract").button({disabled: true});
+			}
+		}
+	);
+};
 
- function replace_form_with_cdata(cdata_arr) {
-	 var xml_text = $("#xml").html();
-	 // basic replacement
-	 var regex = new RegExp("<br>", "g");
-	 xml_text = xml_text.replace(regex, "");
-	 regex = new RegExp("&nbsp;", "g");
-	 xml_text = xml_text.replace(regex, " ");
-	 regex = new RegExp("    ", "g");
-	 xml_text = xml_text.replace(regex, "\t");
-	 regex = new RegExp("&lt;", "g");
-	 xml_text = xml_text.replace(regex, "<");
-	 regex = new RegExp("&gt;", "g");
-	 xml_text = xml_text.replace(regex, ">");
+// setacl button event handler
+var setAclHandler = function() {
+	$("setacl").val("ACL 설정 중");
+	var feed_name = $("#feed_name").val();
+	var parent_name = $("#dir_list").val();
+	var sample_feed = $("#feed_list option:selected").val();
+	$.post(
+		ajax_url,
+		{ "command": "setacl", "feed_name": feed_name, "parent_name": parent_name, "sample_feed": sample_feed },
+		function(data, textStatus, jqXHR) {
+			res = jQuery.parseJSON(data);
+			if (res["result"] != "0") {
+				$("#status").html(get_error_message(res["message"]));
+			} else {
+				$("#status").html(get_success_message("ACL 설정 성공"));
+				$("#setacl").val("ACL 설정 완료");
+				$("#setacl").button({disabled: true});
+				$("#feedly_link").attr('href', 'http://feedly.com/#subscription%2Ffeed%2Fhttp%3A%2F%2Fterzeron.net%2F' + feed_name + '.xml');
+				$("#feedly_link").css('display', 'block');
+			}
+		}
+	);
+};
 
-	 // split
-	 var index = 0;
-	 var i = 0;
-	 var index_arr = new Array();
-	 var start_pattern_str = "<![CDATA[";
-	 var end_pattern_str = "]]>";
-	 while (1) {
-		 index = xml_text.indexOf(start_pattern_str, index);
-		 if (index < 0 || index >= xml_text.length) {
-			 break;
-		 }
-		 // 시작점
-		 index_arr.push(index);
-		 index = xml_text.indexOf(end_pattern_str, index);
-		 if (index < 0 || index >= xml_text.length) {
-			 break;
-		 }
-		 // 종료점
-		 index_arr.push(index);
-	 }
-	 // concatenate
-	 var new_xml_text = new StringBuffer();
-	 var start = 0;
-	 var j = 0;
-	 for (var i = 0; i < index_arr.length; i+=2) {
-		 // (시작점, 종료점) 두 개씩 꺼내서 그 가운데를 바꿔치기함
-		 var i0 = index_arr[i];
-		 var i1 = index_arr[i+1];
-		 new_xml_text.append(xml_text.substr(start, i0 + start_pattern_str.length - start));
-		 new_xml_text.append(cdata_arr[j++]);
-		 new_xml_text.append(xml_text.substr(i1, end_pattern_str.length));
-		 start = i1 + 3;
-	 }
-	 new_xml_text.append(xml_text.substr(start, xml_text.length - start));
-	 return new_xml_text.toString();
- }
+// reset button event handler
+var resetHandler = function() {
+ 	$("#save").val("저장");
+	$("#save").button({disabled: false})
+ 		$("#lint").val("XML lint 실행");
+	$("#lint").button({disabled: false})
+ 		$("#extract").val("추출 실행");
+	$("#extract").button({disabled: false})
+ 		$("#setacl").val("ACL 설정");
+	$("#setacl").button({disabled: false})
+ 		$("#disable").val("비활성화");
+	$("#disable").button({disabled: false})
+ 		$("#remove").val("삭제");
+	$("#remove").button({disabled: false})
+		$("#feedly_link").css('display', 'hidden');
+};
+
+// remove button event handler
+var removeHandler = function() {
+	$("remove").val("삭제 중");
+	//var feed_name = $("#feed_name").val();
+	var parent_name = $("#dir_list").val();
+	var sample_feed = $("#feed_list option:selected").val();
+	$.post(
+		ajax_url,
+		{ "command": "remove", "parent_name": parent_name, "sample_feed": sample_feed },
+		function(data, textStatus, jqXHR) {
+			res = jQuery.parseJSON(data);
+			if (res["result"] != "0") {
+				$("#status").html(get_error_message(res["message"]));
+			} else {
+				$("#status").html(get_success_message("삭제 성공"));
+				$("#remove").val("삭제 완료");
+				$("#remove").button({disabled: true});
+			}
+		}
+	);
+};
+
+// remove button event handler
+var disableHandler = function() {
+	$("disable").val("비활성화 중");
+	//var feed_name = $("#feed_name").val();
+	var parent_name = $("#dir_list").val();
+	var sample_feed = $("#feed_list option:selected").val();
+	$.post(
+		ajax_url,
+		{ "command": "disable", "parent_name": parent_name, "sample_feed": sample_feed },
+		function(data, textStatus, jqXHR) {
+			res = jQuery.parseJSON(data);
+			if (res["result"] != "0") {
+				$("#status").html(get_error_message(res["message"]));
+			} else {
+				$("#status").html(get_success_message("비활성화 성공"));
+				$("#disable").val("비활성화 완료");
+				$("#disable").button({disabled: true});
+			}
+		}
+	);
+};
+
+$("#save").button().click(saveHandler);
+$("#lint").button().click(lintHandler);
+$("#extract").button().click(extractHandler);
+$("#setacl").button().click(setAclHandler);
+$("#reset").button().click(resetHandler);
+$("#remove").button().click(removeHandler);
+$("#disable").button().click(disableHandler);
+
+function read_form_cdata() {
+	var cdata_arr = new Array();
+	var cdata = $(".cdata");
+	for (var i = 0; i < cdata.length; i++) {
+		cdata_arr[i] = cdata[i].value;
+	}
+	return cdata_arr;
+}
+
+var StringBuffer = function() {
+	this.buffer = new Array();
+}
+StringBuffer.prototype.append = function(obj) {
+	this.buffer.push(obj);
+}
+StringBuffer.prototype.toString = function() {
+	return this.buffer.join("");
+}
+
+function replace_form_with_cdata(cdata_arr) {
+	var xml_text = $("#xml").html();
+	// basic replacement
+	var regex = new RegExp("<br>", "g");
+	xml_text = xml_text.replace(regex, "");
+	regex = new RegExp("&nbsp;", "g");
+	xml_text = xml_text.replace(regex, " ");
+	regex = new RegExp("    ", "g");
+	xml_text = xml_text.replace(regex, "\t");
+	regex = new RegExp("&lt;", "g");
+	xml_text = xml_text.replace(regex, "<");
+	regex = new RegExp("&gt;", "g");
+	xml_text = xml_text.replace(regex, ">");
+
+	// split
+	var index = 0;
+	var i = 0;
+	var index_arr = new Array();
+	var start_pattern_str = "<![CDATA[";
+	var end_pattern_str = "]]>";
+	while (1) {
+		index = xml_text.indexOf(start_pattern_str, index);
+		if (index < 0 || index >= xml_text.length) {
+			break;
+		}
+		// 시작점
+		index_arr.push(index);
+		index = xml_text.indexOf(end_pattern_str, index);
+		if (index < 0 || index >= xml_text.length) {
+			break;
+		}
+		// 종료점
+		index_arr.push(index);
+	}
+	// concatenate
+	var new_xml_text = new StringBuffer();
+	var start = 0;
+	var j = 0;
+	for (var i = 0; i < index_arr.length; i+=2) {
+		// (시작점, 종료점) 두 개씩 꺼내서 그 가운데를 바꿔치기함
+		var i0 = index_arr[i];
+		var i1 = index_arr[i+1];
+		new_xml_text.append(xml_text.substr(start, i0 + start_pattern_str.length - start));
+		new_xml_text.append(cdata_arr[j++]);
+		new_xml_text.append(xml_text.substr(i1, end_pattern_str.length));
+		start = i1 + 3;
+	}
+	new_xml_text.append(xml_text.substr(start, xml_text.length - start));
+	return new_xml_text.toString();
+}
 </script>
