10e4b30617eb16a8cc50ccc4dffdcd1b93bc7abe
Young-il Cho <terzeron@gmail.com>
2015-08-26 17:04:31 +0900

    extend the refresh interval from 144 mins to 3 days

--- bin/all.sh
+++ bin/all.sh
@@ -2,34 +2,20 @@ date
 start_ts=`date +"%s"`
 
 max_idx=1000
-. /Users/terzeron/.bashrc
-script_path=$(dirname $0)
-#echo script_path=${script_path}
-. ${script_path}/setup.sh
-#echo FEED_MAKER_HOME=${FEED_MAKER_HOME}
-
 runlog=run.log
 errorlog=error.log
 
-function send_error_msg
-{
-    #id="terzeron"
-    #curl -Sso/dev/null "http://ssea.naver.com:20000/smsg?id=$id&method=email&subject=feedmaker&msg=$1"
-	#echo "curl -Sso/dev/null http://ssea.naver.com:20000/smsg?id=$id&method=email&subject=feedmaker&msg=$1"
-	#echo "$1" | mail -s "feedmaker error" terzeron@gmail.com
-    #echo "send error message '$1'"
-	#echo "email function is not supported"
+function send_error_msg() {
 	echo "$1" | mail -s "FeedMaker error" 5jkfokr6ggh8@bxc.io
 }
 
-function execute_job
-{
+function execute_job() {
 	dir=$1
 	#echo $dir
 	if [ -d "$dir" -a -f "$dir/conf.xml" ]; then
 		echo -n $dir "  "
 		is_completed=$(grep "<is_completed>true" $dir/conf.xml)
-		recent_collection_list=$([ -e "$dir/newlist" ] && find $dir/newlist -type f -mmin 144)
+		recent_collection_list=$([ -e "$dir/newlist" ] && find $dir/newlist -type f -mtime +72)
 		rm -f $runlog $errorlog
 		if [ "$is_completed" != "" -a "$recent_collection_list" == "" ]; then
 			(cd $dir; run.sh -c > $runlog 2> $errorlog; run.sh >> $runlog 2>> $errorlog)
