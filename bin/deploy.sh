38f6409200a2dde3aab725ca2f87380899f1735b
Young-il Cho <terzeron@gmail.com>
2015-08-28 14:25:02 +0900

    modify deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,13 +4,36 @@ FEED_MAKER_HOME=/Users/terzeron/workspace/fm
 FEED_MAKER_HOME_DEV=/Users/terzeron/workspace/fmd
 
 cd $FEED_MAKER_HOME_DEV
+pwd
+echo 
+
+echo "git checkout master"
 git checkout master
+echo
+
+echo "git pull"
 git pull
+echo
+
+echo "git merge develop"
 git merge develop
-git pull
+echo
+
+echo "git push"
 git push
+echo 
+
+echo "git checkout develop"
 git checkout develop
 
 cd $FEED_MAKER_HOME
+pwd
+echo
+
+echo "git checkout master"
 git checkout master
+echo
+
+echo "git pull"
 git pull
+echo
