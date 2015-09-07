1081992f730f85ea5fd944f6206f63e20d660730
Young-il Cho <terzeron@gmail.com>
2015-09-07 12:19:41 +0900

    add a script for the markdown to html conversion

---
+++ bin/markdown_to_html.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+import os
+import sys
+import feedmakerutil
+
+
+def main():
+    cmd = "markdown"
+    print(feedmakerutil.execCmd(cmd))
+    
+
+if __name__ == "__main__":
+    main()
