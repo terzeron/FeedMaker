c7f194d311fdaf12dcabd53322f9fa93e46ee4ac
Youngil Cho <terzeron@gmail.com>
2016-12-17 21:43:03 +0900

    adjust indentation

--- bin/extract_element.py
+++ bin/extract_element.py
@@ -17,79 +17,79 @@ signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
 
 def extract_content(config_item):
-	#print "# extract_content()"
-	# configuration
-	class_str = ""
-	id_str = ""
-	path_str = ""
-	config = feedmakerutil.readConfig()
-	if config == None:
-		return -1
-	if config_item in ("collection", "extraction"):
-		conf_node = feedmakerutil.getConfigNode(config, config_item)
-		if conf_node == None:
-			die("can't find '%s' element from configuration" % config_item)
-		id_str = feedmakerutil.getConfigValue(conf_node, "element_id")
-		class_str = feedmakerutil.getConfigValue(conf_node, "element_class")
-		path_str = feedmakerutil.getConfigValue(conf_node, "element_path")
-		enc = feedmakerutil.getConfigValue(conf_node, "encoding")
-		#print "# element_id:", id_str
-		#print "# element_class:", class_str
-		#print "# element_path:", path_str
-		#print "# encoding:", enc
-	else:
-		return -1
-	
-	# read html contents
-	html = feedmakerutil.readStdin()
+    #print "# extract_content()"
+    # configuration
+    class_str = ""
+    id_str = ""
+    path_str = ""
+    config = feedmakerutil.readConfig()
+    if config == None:
+        return -1
+    if config_item in ("collection", "extraction"):
+        conf_node = feedmakerutil.getConfigNode(config, config_item)
+        if conf_node == None:
+            die("can't find '%s' element from configuration" % config_item)
+        id_str = feedmakerutil.getConfigValue(conf_node, "element_id")
+        class_str = feedmakerutil.getConfigValue(conf_node, "element_class")
+        path_str = feedmakerutil.getConfigValue(conf_node, "element_path")
+        enc = feedmakerutil.getConfigValue(conf_node, "encoding")
+        #print "# element_id:", id_str
+        #print "# element_class:", class_str
+        #print "# element_path:", path_str
+        #print "# encoding:", enc
+    else:
+        return -1
+    
+    # read html contents
+    html = feedmakerutil.readStdin()
 
-	# sanitize
-	html = re.sub(r'alt="(.*)<br>(.*)"', r'alt="\1 \2"', html);
-	html = re.sub(r'<br>', r'<br/>', html)
+    # sanitize
+    html = re.sub(r'alt="(.*)<br>(.*)"', r'alt="\1 \2"', html);
+    html = re.sub(r'<br>', r'<br/>', html)
     html = re.sub('\x08', '', html)
-	html = re.sub(r'<\?xml[^>]+>', r'', html)
+    html = re.sub(r'<\?xml[^>]+>', r'', html)
 
-	if (class_str == None or class_str == "") and (id_str == None or id_str == "") and (path_str == None or path_str == ""):
-		print(html)
-		return
-	
-	ret = 0
-	for parser in [ "html.parser", "html5lib", "lxml" ]:
-		soup = BeautifulSoup(html, parser)
-		if soup == None:
-			print("can't parse html")
-			print(html)
-			sys.exit(-1)	
-	
-		if id_str != None and id_str != "":
-			divs = soup.find_all(attrs={"id":id_str})
-			if divs:
-				for div in divs:
-					print(div)
-					ret = 1
-		if class_str != None and class_str != "":
-			divs = soup.find_all(class_=class_str)
-			if divs:
-				for div in divs:
-					print(div)
-					ret = 1
-		if path_str != None and path_str != "":
-			divs = getNodeWithPath(soup, path_str)
-			if divs:
-				for div in divs:
-					print(div)
-					ret = 1
-		if ret > 0:
-			break
+    if (class_str == None or class_str == "") and (id_str == None or id_str == "") and (path_str == None or path_str == ""):
+        print(html)
+        return
+    
+    ret = 0
+    for parser in [ "html.parser", "html5lib", "lxml" ]:
+        soup = BeautifulSoup(html, parser)
+        if soup == None:
+            print("can't parse html")
+            print(html)
+            sys.exit(-1)    
+    
+        if id_str != None and id_str != "":
+            divs = soup.find_all(attrs={"id":id_str})
+            if divs:
+                for div in divs:
+                    print(div)
+                    ret = 1
+        if class_str != None and class_str != "":
+            divs = soup.find_all(class_=class_str)
+            if divs:
+                for div in divs:
+                    print(div)
+                    ret = 1
+        if path_str != None and path_str != "":
+            divs = getNodeWithPath(soup, path_str)
+            if divs:
+                for div in divs:
+                    print(div)
+                    ret = 1
+        if ret > 0:
+            break
 
 
 def print_usage(program_name):
-	print(("Usage:\t%s\t<config item>\n" % program_name))
-	print("")
+    print(("Usage:\t%s\t<config item>\n" % program_name))
+    print("")
 
 
 if __name__ == "__main__":
-	if len(sys.argv) < 2:
-		print_usage(sys.argv[0])
-		sys.exit(-1)
-	extract_content(sys.argv[1])
+    if len(sys.argv) < 2:
+        print_usage(sys.argv[0])
+        sys.exit(-1)
+    extract_content(sys.argv[1])
