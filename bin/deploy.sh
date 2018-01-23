bde60b6f059852b1635995556f10239a3eceeaf6
Youngil Cho <terzeron@gmail.com>
2018-01-23 19:25:25 +0900

    add verbose messages

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,13 +4,36 @@ FEED_MAKER_HOME=/Users/terzeron/workspace/fm
 FEED_MAKER_HOME_DEV=/Users/terzeron/workspace/fmd
 
 cd $FEED_MAKER_HOME_DEV
+pwd
+echo
+
+echo "--------------------"
+echo "git checkout master"
 git checkout master
+echo
+
+echo "--------------------"
+echo "git pull"
 git pull
 git merge develop --no-edit
 git pull
 git push
+echo
+
+echo "--------------------"
+echo "git checkout develop"
 git checkout develop
 
 cd $FEED_MAKER_HOME
+pwd
+echo
+
+echo "--------------------"
+echo "git checkout master"
 git checkout master
+echo
+
+echo "--------------------"
+echo "git pull"
 git pull
+echo
