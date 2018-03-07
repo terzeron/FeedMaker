acbfcd565e2037299b96cd5951c9ad48667f3d2f
Youngil Cho <terzeron@gmail.com>
2018-03-07 15:22:23 +0900

    do refactoring on download_image.py

--- bin/download_image.py
+++ bin/download_image.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 import sys
 import os
 import re
@@ -7,31 +8,17 @@ import time
 import feedmakerutil
 from feedmakerutil import IO, URL, Cache
 from logger import Logger
+from typing import List, Optional
 
 
 logger = Logger("download_image.py")
 
 
-def get_cache_file_name(path_prefix, img_url, img_ext, postfix=None, index=None):
-    postfix_str = ""; 
-    if postfix:
-        postfix_str = "_" + postfix
-    
-    index_str = ""
-    if index:
-        index_str = "." + index
-                         
-    if re.search(r'^https?://', img_url) and img_ext:
-        return path_prefix + "/" + URL.get_short_md5_name(img_url) + postfix_str + index_str + "." + img_ext
-     
-    return path_prefix + "/" + img_url
-
-
-def download_image(path_prefix, img_url, img_ext, page_url):
-    cache_file = get_cache_file_name(path_prefix, img_url, img_ext)
+def download_image(path_prefix: str, img_url: str, img_ext: str, page_url: str) -> Optional[str]:
+    cache_file: str = Cache.get_cache_file_name(path_prefix, img_url, img_ext)
     if os.path.isfile(cache_file) and os.stat(cache_file).st_size > 0:
         return True
-    cmd = 'crawler.sh --download "%s" --referer "%s" "%s"' % (cache_file, page_url, img_url)
+    cmd: str = 'crawler.sh --download "%s" --referer "%s" "%s"' % (cache_file, page_url, img_url)
     logger.debug("<!-- %s -->" % (cmd))
     (result, error) = feedmakerutil.exec_cmd(cmd)
     logger.debug("<!-- %s -->" % (result))
@@ -45,30 +32,26 @@ def download_image(path_prefix, img_url, img_ext, page_url):
     return False
 
 
-def main():
-    cmd = "basename " + os.getcwd()
+def main() -> int:
+    cmd: str = "basename " + os.getcwd()
     (result, error) = feedmakerutil.exec_cmd(cmd)
-    feed_name = result.rstrip()
-    img_url_prefix = "http://terzeron.net/xml/img/" + feed_name
-    path_prefix = os.environ["FEED_MAKER_WWW_FEEDS"] + "/img/" + feed_name
+    feed_name: str = result.rstrip()
+    img_url_prefix: str = "http://terzeron.net/xml/img/" + feed_name
+    path_prefix: str = os.environ["FEED_MAKER_WWW_FEEDS"] + "/img/" + feed_name
 
-    img_prefix = ""
-    img_index = -1
-    img_ext = "jpg"
-    num_units = 25
-
-    _url = sys.argv[1]
-    cmd = ""
-    result = ""
+    img_ext: str = "jpg"
+    page_url: str = sys.argv[1]
+    cmd: str = ""
+    result: str = ""
 
     feedmakerutil.make_path(path_prefix)
 
-    line_list = IO.read_stdin_as_line_list()
+    line_list: List[str] = IO.read_stdin_as_line_list()
     for line in line_list:
-        line = line.rstrip()
+        line: str = line.rstrip()
         m = re.search(r'''
         (?P<pre_text>.*)
-        <img 
+        <img
         \s*
         src=
         (["\'])
@@ -79,18 +62,18 @@ def main():
         (?P<post_text>.*)
         ''', line, re.VERBOSE)
         if m:
-            pre_text = m.group("pre_text")
-            img_url = m.group("img_url")
-            post_text = m.group("post_text")
-            
+            pre_text: str = m.group("pre_text")
+            img_url: str = m.group("img_url")
+            post_text: str = m.group("post_text")
+           
             m = re.search(r'^\s*$', pre_text)
             if not m:
                 print(pre_text)
 
             # download
-            cache_file = download_image(path_prefix, img_url, img_ext, _url)
+            cache_file: str = download_image(path_prefix, img_url, img_ext, page_url)
             if cache_file:
-                cache_url = Cache.get_cache_url(img_url_prefix, img_url, img_ext)
+                cache_url: str = Cache.get_cache_url(img_url_prefix, img_url, img_ext)
                 logger.debug("<!-- %s -> %s / %s -->" % (img_url, cache_file, cache_url))
                 print("<img src='%s'/>" % (cache_url))
             else:
@@ -102,6 +85,8 @@ def main():
         else:
             print(line)
 
-            
+    return 0
+
+
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
