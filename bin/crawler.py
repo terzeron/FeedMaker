105b37b4ec48f0766511182d8e8fc0faa438cb50
Youngil Cho <terzeron@gmail.com>
2019-11-04 19:09:26 +0900

    add a functionality which avoids being detected as headless browser

--- bin/crawler.py
+++ bin/crawler.py
@@ -37,10 +37,16 @@ class HeadlessBrowser:
         options.add_argument("--window-size=1920x1080")
         options.add_argument("--disable-web-security")
         options.add_argument("--allow-running-insecure-content")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--lang=ko_KR")
+        options.add_argument("--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
         chrome_driver = "chromedriver"
         driver = webdriver.Chrome(options=options, executable_path=chrome_driver)
 
         driver.get(url)
+        driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: function() {return[1, 2, 3, 4, 5];},});")
+        driver.execute_script("Object.defineProperty(navigator, 'languages', {get: function() {return ['ko-KR', 'ko']}})")
+        #driver.execute_script("const getParameter = WebGLRenderingContext.getParameter;WebGLRenderingContext.prototype.getParameter = function(parameter) {if (parameter === 37445) {return 'NVIDIA Corporation'} if (parameter === 37446) {return 'NVIDIA GeForce GTX 980 Ti OpenGL Engine';}return getParameter(parameter);};")
         if self.sleep_time:
             # necessary to circumvent javascript challenge of cloudflare
             time.sleep(self.sleep_time)
