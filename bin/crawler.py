1c66ad4c34522bf0ac9077fecccb39d3ec399886
Youngil Cho <terzeron@gmail.com>
2020-09-07 10:42:17 +0900

    add pre-defined user-agent header to crawler in case of having no user-agent information

--- bin/crawler.py
+++ bin/crawler.py
@@ -176,9 +176,10 @@ class Crawler():
         LOGGER.debug("Crawler(render_js=%r, method=%r, headers=%r, timeout=%d, num_retries=%d, encoding=%r, verify_ssl=%r, copy_images_from_canvas=%r)", render_js, method, headers, timeout, num_retries, encoding, verify_ssl, copy_images_from_canvas)
         self.render_js = render_js
         self.num_retries = num_retries
+        if not headers['User-Agent']:
+            headers['User-Agent'] = "Mozillla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
         if render_js:
             # headless browser
-            headers['User-Agent'] = "Mozillla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
             self.headless_browser = HeadlessBrowser(headers=headers, copy_images_from_canvas=copy_images_from_canvas)
         else:
             self.requests_client = RequestsClient(method=method, headers=headers, timeout=timeout, encoding=encoding, verify_ssl=verify_ssl)
