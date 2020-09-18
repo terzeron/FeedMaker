c53d9fbc4427e0380612e70e0cc9de4c31b30136
Youngil Cho <terzeron@gmail.com>
2020-09-19 01:14:40 +0900

    add '-u' option for leaving only unqiue images (and removing duplicate image occurrences)

--- bin/post_process_only_for_images.py
+++ bin/post_process_only_for_images.py
@@ -3,15 +3,40 @@
 
 import re
 import sys
+import getopt
+from typing import List
+
+
+
+def print_usage() -> None:
+    print("Usage:\t%s [ <option> ]" % sys.argv[0])
+    print("options")
+    print("\t-u\t\t\tleave only unique images")
 
 
 def main() -> int:
+    leave_only_unique_images: bool = False
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "u")
+    except getopt.GetoptError:
+        print_usage()
+        sys.exit(-1)
+    for o, _ in opts:
+        if o == "-u":
+            leave_only_unique_images = True
+
+    prev_img_tag: str = ""
     for line in sys.stdin:
         if re.search(r'<(meta|style)', line):
             print(line, end='')
         match = re.search(r'(?P<img_tag><img src=[\'"]?[^\'"]+[\'"]?[^>]*>)', line)
-        if match:
-            print(match.group('img_tag'))
+        if match: 
+            img_tag = match.group("img_tag")
+            if not leave_only_unique_images or img_tag != prev_img_tag:
+                print(img_tag)
+            prev_img_tag = img_tag
+            
     return 0
 
 
