6910a626b37734a1259129914b56e64f4f322ed2
Youngil Cho <terzeron@gmail.com>
2021-02-15 16:25:03 +0900

    fix #78 - add <br> at the end of each line - remove pdf file after processing

--- bin/convert_pdf_to_text.py
+++ bin/convert_pdf_to_text.py
@@ -16,13 +16,14 @@ LOGGER = logging.getLogger()
 
 def main() -> int:
     sys.stdin.read()
+    sys.stdin.flush()
     sys.stdin.close()
-    
+
     if len(sys.argv) > 1:
         if os.path.isfile(sys.argv[1]):
             pdf_file = sys.argv[1]
         elif sys.argv[1].startswith("http"):
-            pdf_file = os.environ["FEED_MAKER_WWW_FEEDS_DIR"] + "/pdf/" + str(os.getpid())
+            pdf_file = os.environ["FEED_MAKER_WWW_FEEDS_DIR"] + "/pdf/" + str(os.getpid()) + ".pdf"
             crawler = Crawler()
             _, error = crawler.run(url=sys.argv[1], download_file=pdf_file)
             if error:
@@ -35,13 +36,15 @@ def main() -> int:
     with open(pdf_file, "rb") as f:
         pdf = pdftotext.PDF(f)
         for page in pdf:
-            page = re.sub(r'^\s+', ' ', page)
+            page = re.sub(r'\n', '<br>\n', page)
+            page = re.sub(r'\s\s+', ' ', page)
             page = re.sub(r'\s{4,}', '  ', page)
             page = re.sub(r'^\s*\S+\s* - ', '\n - ', page)
             page = re.sub(r'(?<=\S)\s*(?P<bullet>[▶•])', '\n\g<bullet>', page)
             print(page)
 
     f.close()
+    os.unlink(pdf_file)
 
     return 0
 
