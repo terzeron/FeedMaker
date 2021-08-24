fcad169f7ea84362c2c9633048c4c502032b25ad
Youngil Cho <terzeron@gmail.com>
2021-08-24 15:57:31 +0900

    add a frontend module based on 'quasar' for test modify .gitignore

---
+++ testweb/src/pages/FeedManagement.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>피드 관리</div>
+</template>
+
+<script>
+export default {
+  name: 'ExecResult'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ testweb/src/pages/Problems.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>문제점</div>
+</template>
+
+<script>
+export default {
+  name: 'ExecResult'
+}
+</script>
+
+<style scoped>
+
+</style>
