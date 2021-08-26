a3bfe27038ab4e266316c4057f0726d1fa8cf335
Youngil Cho <terzeron@gmail.com>
2021-08-27 08:57:01 +0900

    remove dev.setup.sh and integrate it to setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,22 +1,26 @@
 # You should change this environment variables according to your environment.
 
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
+  eval "$(pyenv init --path)"
+  eval "$(pyenv init -)" > /dev/null 2>&1
+  eval "$(pyenv virtualenv-init -)"
 fi
 
 # engine directory
-export FEED_MAKER_HOME_DIR=$HOME/workspace/fm
+export FEED_MAKER_HOME_DIR=$(dirname $(dirname $0))
+echo $FEED_MAKER_HOME_DIR
 # feed working directory
-export FEED_MAKER_WORK_DIR=$HOME/workspace/fma
+export FEED_MAKER_WORK_DIR=$(dirname $FEED_MAKER_HOME_DIR)/fma
+echo $FEED_MAKER_WORK_DIR
 export FEED_MAKER_LOG_DIR=$FEED_MAKER_WORK_DIR/logs
 # admin console web page directory
 export FEED_MAKER_WWW_ADMIN_DIR=$HOME/public_html/fm
 # feed directory public to feed crawlers
 export FEED_MAKER_WWW_FEEDS_DIR=$HOME/public_html/xml
 # CartoonSplit utilities
-export CARTOON_SPLIT_HOME_DIR=$HOME/workspace/CartoonSplit
+export CARTOON_SPLIT_HOME_DIR=$HOME/workspace1/cs.dev
 
 export PATH=$FEED_MAKER_HOME_DIR/bin:$CARTOON_SPLIT_HOME_DIR:$PATH
 export PYTHONPATH=$FEED_MAKER_HOME_DIR/bin:$PYTHONPATH
-
