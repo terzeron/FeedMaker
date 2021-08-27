0d66be56f74bbdb9a335bc6d71beccf962f28269
Youngil Cho <terzeron@gmail.com>
2021-08-27 13:51:18 +0900

    fix a bug in specifying FEED_MAKER_HOME_DIR from path of this script

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,7 +9,7 @@ if command -v pyenv 1>/dev/null 2>&1; then
 fi
 
 # engine directory
-export FEED_MAKER_HOME_DIR=$(dirname $(dirname $0))
+export FEED_MAKER_HOME_DIR=$(dirname $(readlink -f $0))
 # feed working directory
 export FEED_MAKER_WORK_DIR=$(dirname $FEED_MAKER_HOME_DIR)/fma
 export FEED_MAKER_LOG_DIR=$FEED_MAKER_WORK_DIR/logs
@@ -18,7 +18,7 @@ export FEED_MAKER_WWW_ADMIN_DIR=$HOME/public_html/fm
 # feed directory public to feed crawlers
 export FEED_MAKER_WWW_FEEDS_DIR=$HOME/public_html/xml
 # CartoonSplit utilities
-export CARTOON_SPLIT_HOME_DIR=$HOME/workspace1/cs.dev
+export CARTOON_SPLIT_HOME_DIR=$HOME/workspace/cs.dev
 
 export PATH=$FEED_MAKER_HOME_DIR/bin:$CARTOON_SPLIT_HOME_DIR:$PATH
 export PYTHONPATH=$FEED_MAKER_HOME_DIR/bin:$PYTHONPATH
