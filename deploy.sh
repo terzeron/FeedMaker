dbbbd21954dcf3c5d751b3408f0b33da96c70cbc
Youngil Cho <terzeron@gmail.com>
2021-08-30 17:18:11 +0900

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,16 @@
 
 (cd frontend; npm run build) && scp -r -P 20023 frontend/dist terzeron.com:~/public_html/fmw/frontend/
 scp -r -P 20023 backend/*.py backend/index.wsgi backend/requirements.txt terzeron.com:~/public_html/fmw/backend/
-git switch develop
-git push
-git switch master
-git merge develop
-git switch develop
+
+echo "git switch develop"
+git switch develop > /dev/null
+echo "git push"
+git push > /dev/null
+echo "git switch master"
+git switch master > /dev/null
+echo "git merge develop"
+git merge develop > /dev/null
+echo "git push"
+git push > /dev/null
+echo "git switch develop"
+git switch develop > /dev/null
