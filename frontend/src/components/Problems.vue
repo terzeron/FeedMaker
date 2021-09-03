91e16378dae2f0f9f1b58d03dafed77485a08eac
Youngil Cho <terzeron@gmail.com>
2021-09-03 19:36:14 +0900

    change list group to table filter public_feed_info_list with size condition

--- frontend/src/components/Problems.vue
+++ frontend/src/components/Problems.vue
@@ -1,122 +1,158 @@
 <template>
   <b-container fluid>
     <b-row>
-      <b-col cols="12">
-        <b-card
-            title="피드 상태"
-            class="">
+      <b-col cols="12" class="m-0 p-0">
+        <b-card-header header-bg-variant="dark" header-text-variant="white" header-tag="header">피드 상태</b-card-header>
+        <b-card-body class="m-0 p-0">
           <b-table
+              class="m-0 p-0 text-break"
               striped
               hover
-              responsive
               small
               :fields="status_info_fields"
               :items="status_info_list"
               :sort-by.sync="status_info_sort_by"
               :sort-desc.sync="status_info_sort_desc">
           </b-table>
-        </b-card>
+        </b-card-body>
       </b-col>
 
-      <b-col cols="12" lg="8">
-        <b-card
-            title="점진적 피딩 진행률"
-            class="">
+      <b-col cols="12" lg="8" class="m-0 p-0">
+        <b-card-header header-bg-variant="dark" header-text-variant="white" header-tag="header">점진적 피딩 진행률
+        </b-card-header>
+        <b-card-body class="m-0 p-0">
           <b-table
+              class="m-0 p-0"
               striped
               hover
-              responsive
               small
               :fields="progress_info_fields"
               :items="progress_info_list"
               :sort-by.sync="progress_info_sort_by"
               :sort-desc.sync="progress_info_sort_desc">
           </b-table>
-        </b-card>
+        </b-card-body>
       </b-col>
 
-      <b-col cols="12" lg="4">
-        <b-card
-            title="이미지가 누락된 HTML 파일"
-            class="">
-          <b-list-group
-              v-for="info in html_file_with_image_not_found_list"
-              :key="info.name">
-            <b-list-group-item>{{ info.name }} : {{ info.count }}</b-list-group-item>
-          </b-list-group>
-        </b-card>
+      <b-col cols="12" lg="4" class="m-0 p-0">
+        <b-card-header header-bg-variant="dark" header-text-variant="white" header-tag="header">이미지가 누락된 HTML 파일
+        </b-card-header>
+        <b-card-body class="m-0 p-0">
+          <b-table
+              class="m-0 p-0 text-break"
+              striped
+              hover
+              small
+              :fields="html_file_with_image_not_found_fields"
+              :items="html_file_with_image_not_found_list"
+              :sort-by.sync="html_file_with_image_not_found_sort_by"
+              :sort-desc.sync="html_file_with_image_not_found_sort_desc">
+          </b-table>
+        </b-card-body>
       </b-col>
 
-      <b-col cols="12" lg="4">
-        <b-card
-            title="이미지 태그가 없는 HTML 파일"
-            class="">
-          <b-list-group
-              v-for="info in html_file_without_image_tag_list"
-              :key="info.name">
-            <b-list-group-item>{{ info.name }} : {{ info.count }}</b-list-group-item>
-          </b-list-group>
-        </b-card>
+      <b-col cols="12" lg="4" class="m-0 p-0">
+        <b-card-header header-bg-variant="dark" header-text-variant="white" header-tag="header">이미지 태그가 없는 HTML
+          파일
+        </b-card-header>
+        <b-card-body class="m-0 p-0">
+          <b-table
+              class="m-0 p-0 text-break"
+              striped
+              hover
+              small
+              :fields="html_file_without_image_tag_fields"
+              :items="html_file_without_image_tag_list"
+              :sort-by.sync="html_file_without_image_tag_sort_by"
+              :sort-desc.sync="html_file_without_image_tag_sort_desc">
+          </b-table>
+        </b-card-body>
       </b-col>
 
-      <b-col cols="12" lg="4">
-        <b-card
-            title="이미지 태그가 많은 HTML 파일"
-            class="">
-          <b-list-group
-              v-for="info in html_file_with_many_image_tag_list"
-              :key="info.name">
-            <b-list-group-item>{{ info.name }} : {{ info.count }}</b-list-group-item>
-          </b-list-group>
-        </b-card>
+      <b-col cols="12" lg="4" class="m-0 p-0">
+        <b-card-header header-bg-variant="dark" header-text-variant="white" header-tag="header">이미지 태그가 많은 HTML 파일
+        </b-card-header>
+        <b-card-body class="m-0 p-0">
+          <b-table
+              class="m-0 p-0 text-break"
+              striped
+              hover
+              small
+              :fields="html_file_with_many_image_tag_fields"
+              :items="html_file_with_many_image_tag_list"
+              :sort-by.sync="html_file_with_many_image_tag_sort_by"
+              :sort-desc.sync="html_file_with_many_image_tag_sort_desc">
+          </b-table>
+        </b-card-body>
       </b-col>
 
-      <b-col cols="12" lg="4">
-        <b-card
-            title="파일 크기가 너무 작은 HTML 파일"
-            class="">
-          <b-list-group
-              v-for="info in html_file_size_list"
-              :key="info.name">
-            <b-list-group-item>{{ info.name }} : {{ info.size }}</b-list-group-item>
-          </b-list-group>
-        </b-card>
+      <b-col cols="12" lg="4" class="m-0 p-0">
+        <b-card-header header-bg-variant="dark" header-text-variant="white" header-tag="header">파일 크기가 너무 작은 HTML 파일
+        </b-card-header>
+        <b-card-body class="m-0 p-0">
+          <b-table
+              class="m-0 p-0 text-break"
+              striped
+              hover
+              small
+              :fields="html_file_size_fields"
+              :items="html_file_size_list"
+              :sort-by.sync="html_file_size_sort_by"
+              :sort-desc.sync="html_file_size_sort_desc">
+          </b-table>
+        </b-card-body>
       </b-col>
 
-      <b-col cols="12" lg="4">
-        <b-card
-            title="결과 파일의 크기"
-            class="">
-          <b-list-group
-              v-for="info in public_feed_info_list"
-              :key="info.name">
-            <b-list-group-item>{{ info.name }} : {{ info.size }}</b-list-group-item>
-          </b-list-group>
-        </b-card>
+      <b-col cols="12" lg="4" class="m-0 p-0">
+        <b-card-header header-bg-variant="dark" header-text-variant="white" header-tag="header">결과(public feed) 파일의 크기
+        </b-card-header>
+        <b-card-body class="m-0 p-0">
+          <b-table
+              class="m-0 p-0"
+              striped
+              hover
+              small
+              :fields="public_feed_info_fields"
+              :items="public_feed_info_list"
+              :sort-by.sync="public_feed_info_sort_by"
+              :sort-desc.sync="public_feed_info_sort_desc">
+          </b-table>
+        </b-card-body>
       </b-col>
 
-      <b-col cols="12" lg="4">
-        <b-card
-            title="피드별 list_url element 갯수"
-            class="">
-          <b-list-group
-              v-for="info in feed_name_list_url_count_list"
-              :key="info.name">
-            <b-list-group-item>{{ info.name }} : {{ info.count }}</b-list-group-item>
-          </b-list-group>
-        </b-card>
+      <b-col cols="12" lg="4" class="m-0 p-0">
+        <b-card-header header-bg-variant="dark" header-text-variant="white" header-tag="header">피드별 list_url element
+          갯수
+        </b-card-header>
+        <b-card-body class="m-0 p-0">
+          <b-table
+              class="m-0 p-0"
+              striped
+              hover
+              small
+              :fields="list_url_info_fields"
+              :items="list_url_info_list"
+              :sort-by.sync="list_url_info_sort_by"
+              :sort-desc.sync="list_url_info_sort_desc">
+          </b-table>
+        </b-card-body>
       </b-col>
 
-      <b-col cols="12" lg="4">
-        <b-card
-            title="element별 사용 횟수"
-            class="">
-          <b-list-group
-              v-for="info in element_name_count_list"
-              :key="info.name">
-            <b-list-group-item>{{ info.name }} : {{ info.count }}</b-list-group-item>
-          </b-list-group>
-        </b-card>
+      <b-col cols="12" lg="4" class="m-0 p-0">
+        <b-card-header header-bg-variant="dark" header-text-variant="white" header-tag="header">element별 사용 횟수
+        </b-card-header>
+        <b-card-body class="m-0 p-0">
+          <b-table
+              class="m-0 p-0"
+              striped
+              hover
+              small
+              :fields="element_info_fields"
+              :items="element_info_list"
+              :sort-by.sync="element_info_sort_by"
+              :sort-desc.sync="element_info_sort_desc">
+          </b-table>
+        </b-card-body>
       </b-col>
     </b-row>
 
@@ -133,6 +169,7 @@
 
 <script>
 import axios from 'axios';
+import _ from 'lodash';
 
 export default {
   name: 'Problems',
@@ -168,14 +205,61 @@ export default {
       status_info_sort_desc: false,
       status_info_list: [],
 
-      public_feed_info_list: [],
-      access_info_list: [],
-      html_file_size_list: [],
-      html_file_with_many_image_tag_list: [],
-      html_file_without_image_tag_list: [],
+      html_file_with_image_not_found_fields: [
+        {key: 'name', sortable: true},
+        {key: 'count', sortable: true}
+      ],
+      html_file_with_image_not_found_sort_by: 'count',
+      html_file_with_image_not_found_sort_desc: true,
       html_file_with_image_not_found_list: [],
-      feed_name_list_url_count_list: [],
-      element_name_count_list: [],
+
+      html_file_without_image_tag_fields: [
+        {key: 'name', sortable: true},
+        {key: 'count', sortable: true}
+      ],
+      html_file_without_image_tag_sort_by: 'count',
+      html_file_without_image_tag_sort_desc: true,
+      html_file_without_image_tag_list: [],
+
+      html_file_with_many_image_tag_fields: [
+        {key: 'name', sortable: true},
+        {key: 'count', sortable: true}
+      ],
+      html_file_with_many_image_tag_sort_by: 'count',
+      html_file_with_many_image_tag_sort_desc: true,
+      html_file_with_many_image_tag_list: [],
+
+      html_file_size_fields: [
+        {key: 'name', sortable: true},
+        {key: 'size', sortable: true}
+      ],
+      html_file_size_sort_by: 'size',
+      html_file_size_sort_desc: false,
+      html_file_size_list: [],
+
+      public_feed_info_fields: [
+        {key: 'name', sortable: true},
+        {key: 'size', sortable: true}
+      ],
+      public_feed_info_sort_by: 'size',
+      public_feed_info_sort_desc: false,
+      public_feed_info_list: [],
+
+      list_url_info_fields: [
+        {key: 'name', sortable: true},
+        {key: 'count', sortable: true}
+      ],
+      list_url_info_sort_by: 'count',
+      list_url_info_sort_desc: true,
+      list_url_info_list: [],
+
+      element_info_fields: [
+        {key: 'name', sortable: true},
+        {key: 'count', sortable: true}
+      ],
+      element_info_sort_by: 'count',
+      element_info_sort_desc: true,
+      element_info_list: [],
     }
   },
   components: {},
@@ -216,7 +300,9 @@ export default {
       const path3 = this.getApiUrlPath() + '/problems/public_feed_info';
       axios.get(path3)
           .then((res3) => {
-            this.public_feed_info_list = res3.data['result'];
+            this.public_feed_info_list = _.filter(res3.data['result'], function (o) {
+              return parseInt(o['size'], 10) < 4 * 1024;
+            });
           })
           .catch((error) => {
             console.error(error);
@@ -225,8 +311,8 @@ export default {
       const path4 = this.getApiUrlPath() + '/problems/element_info';
       axios.get(path4)
           .then((res4) => {
-            this.feed_name_list_url_count_list = res4.data['result']['feed_name_list_url_count_list'];
-            this.element_name_count_list = res4.data['result']['element_name_count_list'];
+            this.list_url_info_list = res4.data['result']['feed_name_list_url_count_list'];
+            this.element_info_list = res4.data['result']['element_name_count_list'];
           })
           .catch((error) => {
             console.error(error);
