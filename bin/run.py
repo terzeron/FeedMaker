f45a6812647dd22e5741b90a8b0c3b6dc822d2b3
Youngil Cho <terzeron@gmail.com>
2021-09-09 08:49:30 +0900

    Revert "remove utilty functions related to process group from feed_maker_util.py"
    This reverts commit c5abae4a9b6f958510402ae1e8efe25c74436342.

--- bin/run.py
+++ bin/run.py
@@ -12,7 +12,7 @@ import concurrent.futures
 import getopt
 from pathlib import Path
 from typing import Dict, Tuple, List, Any, Set
-from feed_maker_util import Config, exec_cmd, send_error_msg
+from feed_maker_util import Config, exec_cmd, send_error_msg, kill_process_group
 from feed_maker import FeedMaker
 from problem_checker import ProblemChecker
 
@@ -299,6 +299,7 @@ def main() -> int:
     runner = FeedMakerRunner(html_archiving_period=30, list_archiving_period=7)
     if options["do_make_all_feeds"]:
         result = runner.make_all_feeds()
+        kill_process_group("chromedriver")
     else:
         result = runner.make_single_feed(feed_dir_path, options)
 
