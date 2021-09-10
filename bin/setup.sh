9b7c3dfc8b5511e82595c096375969e6fea0308d
Youngil Cho <terzeron@gmail.com>
2021-09-10 09:23:21 +0900

    fix path of cartoon split package

--- bin/setup.sh
+++ bin/setup.sh
@@ -18,7 +18,7 @@ export FEED_MAKER_WWW_ADMIN_DIR=$HOME/public_html/fm
 # feed directory public to feed crawlers
 export FEED_MAKER_WWW_FEEDS_DIR=$HOME/public_html/xml
 # CartoonSplit utilities
-export CARTOON_SPLIT_HOME_DIR=$HOME/workspace/cs.dev
+export CARTOON_SPLIT_HOME_DIR=$HOME/workspace/CartoonSplit
 
 export PATH=$FEED_MAKER_HOME_DIR/bin:$CARTOON_SPLIT_HOME_DIR:$PATH
 export PYTHONPATH=$FEED_MAKER_HOME_DIR/bin:$PYTHONPATH
