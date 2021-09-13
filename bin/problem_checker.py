e2c65773170f62c11bec9d73ebeb10a3fc02035c
Youngil Cho <terzeron@gmail.com>
2021-09-13 20:10:38 +0900

    add count of <item> to public_feed_info_list

--- bin/problem_checker.py
+++ bin/problem_checker.py
@@ -159,13 +159,18 @@ class ProblemChecker:
         for path in self.public_feed_dir.iterdir():
             if path.suffix == ".xml":
                 feed_name = path.stem
-                if not feed_name.startswith("_"):
-                    try:
-                        s = path.stat()
-                        upload_date = datetime.fromtimestamp(s.st_mtime)
-                        self.public_feed_info_list.append({"feed_name": feed_name, "feed_title": self.feed_name_title_map.get(feed_name, ""), "file_path": str(path.relative_to(self.public_feed_dir)), "upload_date": self.convert_datetime_to_str(upload_date), "size": s.st_size})
-                    except Exception as e:
-                        LOGGER.error("can't get stat from public_feed '%s', %s", path, str(e))
+                if feed_name.startswith("_"):
+                    continue
+
+                try:
+                    with path.open(encoding="utf-8") as infile:
+                        file_content = infile.read()
+                        num_items = file_content.count("<item>")
+                    s = path.stat()
+                    upload_date = datetime.fromtimestamp(s.st_mtime)
+                    self.public_feed_info_list.append({"feed_name": feed_name, "feed_title": self.feed_name_title_map.get(feed_name, ""), "file_path": str(path.relative_to(self.public_feed_dir)), "upload_date": self.convert_datetime_to_str(upload_date), "size": s.st_size, "num_items": num_items})
+                except Exception as e:
+                    LOGGER.error("can't get stat from public_feed '%s', %s", path, str(e))
 
         print("* The loading of public feed file is done. %d items" % len(self.public_feed_info_list))
         if do_merge:
