ad3f95982d5537cec3ae10ea64a26a555f7ee75e
Youngil Cho <terzeron@gmail.com>
2022-01-11 14:05:42 +0900

    modify test case of images in canvas for crawler(headless browser)

--- bin/test_crawler2.py
+++ bin/test_crawler2.py
@@ -33,7 +33,7 @@ class CrawlerTest(unittest.TestCase):
         url = "https://terzeron.com/crawler/images_in_canvas_test.html"
         crawler = Crawler(render_js=True, copy_images_from_canvas=True)
         actual, _, _ = crawler.run(url)
-        m = re.search(r'<div class="images_from_canvas"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAABkCAYAAADDhn8LAAAAAX.*KBUgI3O8Nj9LDhwKlP8DuACSYNiipSoAAAAASUVORK5CYII="></div>', actual)
+        m = re.search(r'<div class="images_from_canvas"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMgAAABkCAYAAADDhn8LAAAAAX.*"></div>', actual)
         self.assertTrue(m)
         del crawler
 
