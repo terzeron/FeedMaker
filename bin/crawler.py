97159ae91a69ae646e7167c1bcc519ea612d6157
Youngil Cho <terzeron@gmail.com>
2023-03-08 22:10:33 +0900

    convert warning messages from crawler and headless browser to html comments

--- bin/crawler.py
+++ bin/crawler.py
@@ -73,13 +73,11 @@ class RequestsClient:
                 response = requests.get(self.headers['Referer'], headers=self.headers, timeout=self.timeout, verify=self.verify_ssl, allow_redirects=allow_redirects)
             except requests.exceptions.ConnectionError as e:
                 LOGGER.warning(f"<!-- Warning: can't connect to '{url}' for temporary network error -->")
-                LOGGER.warning("<!-- ")
-                LOGGER.warning(e)
-                LOGGER.warning(" -->")
+                LOGGER.warning("<!-- ", e, " -->")
                 return "", f"can't connect to '{url}' for temporary network error", {}, None
             except requests.exceptions.ReadTimeout as e:
-                LOGGER.warning(f"Warning: can't read data from '{url}' for timeout")
-                LOGGER.warning(e)
+                LOGGER.warning(f"<!-- Warning: can't read data from '{url}' for timeout -->")
+                LOGGER.warning("<-- ", e, " -->")
                 return "", f"Warning: can't read data from '{url}' for timeout", {}, None
             if response.cookies:
                 self.write_cookies_to_file(response.cookies)
@@ -98,11 +96,11 @@ class RequestsClient:
                 response = requests.head(url, headers=self.headers, timeout=self.timeout, verify=self.verify_ssl)
                 return str(response.status_code), "", dict(response.headers), response.status_code
         except requests.exceptions.ConnectionError as e:
-            LOGGER.warning(f"Warning: can't connect to '{url}' for temporary network error")
-            LOGGER.warning(e)
+            LOGGER.warning(f"<!-- Warning: can't connect to '{url}' for temporary network error -->")
+            LOGGER.warning("<!-- ", e, " -->")
             return "", f"can't connect to '{url}' for temporary network error", {}, None
         except requests.exceptions.ReadTimeout as e:
-            LOGGER.warning(f"Warning: can't read data from '{url}' for timeout")
+            LOGGER.warning(f"<!-- Warning: can't read data from '{url}' for timeout -->")
             LOGGER.warning(e)
             return "", f"Warning: can't read data from '{url}' for timeout", {}, None
 
