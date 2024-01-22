341fdaa673d08595c65cdfb0964c54a158346605
Youngil Cho <terzeron@gmail.com>
2024-01-22 11:20:35 +0900

    seperate an copying action of 'CartoonSplit' from FeedMaker git repo

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -d CartoonSplit ]; then
-    (cd CartoonSplit && git pull)
+    (cd CartoonSplit && git stash && git pull && git stash pop)
 else
     git clone https://github.com/terzeron/CartoonSplit
 fi
@@ -9,5 +9,5 @@ fi
 rm -rf */{nohup.out*,run.log*,.mypy_cache,__pycache__,.idea,.git} CartoonSplit/test
 docker build -f Dockerfile.backend -t terzeron/fm_backend .
 
-(cd frontend && npm install && npx browserslist@latest && npm run build)
+(cd frontend && npm install -y && npx browserslist@latest -y && npm run build)
 docker build -f Dockerfile.frontend -t terzeron/fm_frontend .
