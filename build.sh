0f213d23dd781d6456bd23583152d0d9e01c45b4
Youngil Cho <terzeron@gmail.com>
2024-01-22 17:16:42 +0900

    pass environment variable to Dockerfile

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -d CartoonSplit ]; then
-    (cd CartoonSplit && git stash && git pull && git stash pop)
+    (cd CartoonSplit && git stash > /dev/null && git pull > /dev/null && git stash pop > /dev/null)
 else
-    git clone https://github.com/terzeron/CartoonSplit
+    git clone https://github.com/terzeron/CartoonSplit > /dev/null
 fi
 
 rm -rf */{nohup.out*,run.log*,.mypy_cache,__pycache__,.idea,.git} CartoonSplit/test
-docker build -f Dockerfile.backend -t terzeron/fm_backend .
+docker build -f Dockerfile.backend --build-arg FM_BACKEND_PORT="$FM_BACKEND_PORT" -t terzeron/fm_backend .
 
-(cd frontend && npm install -y && npx browserslist@latest -y && npm run build)
+(cd frontend && npm install -y > /dev/null && npx browserslist@latest > /dev/null && npm run build > /dev/null)
 docker build -f Dockerfile.frontend -t terzeron/fm_frontend .
