5fa813f2571e047a61f98038d088b9261f937ccb
Youngil Cho <terzeron@gmail.com>
2025-07-04 15:49:47 +0900

    프론트엔드 빌드 스크립트 수정: 초기화 스크립트 실행 및 컨테이너 배포 관련 안내 메시지 추가로 사용자 편의성 향상.

--- build.sh
+++ build.sh
@@ -17,7 +17,13 @@ docker build -f Dockerfile.frontend -t terzeron/fm_frontend . && \
 docker tag terzeron/fm_frontend:latest registry.terzeron.com/terzeron/fm_frontend:latest && \
 docker push registry.terzeron.com/terzeron/fm_frontend:latest || exit 1
 
+echo
 echo 'You should run the initialization script;'
 echo 'python -c "from bin.db import DB; DB.create_all_tables()"'
 echo 'python -c "from bin.problem_manager import ProblemManager; pm = ProblemManager(); pm.load_all()"'
 
+echo
+echo 'You might deploy the containers;'
+echo 'kubectl apply -f ~/k8s/feedmaker/fm-deployment.yml'
+echo 'kubectl rollout restart deployment fm-backend -n feedmaker'
+echo 'kubectl rollout restart deployment fm-frontend -n feedmaker'
