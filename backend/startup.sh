5326aa1e77123da0a224361d8426d96cbd877650
Youngil Cho <terzeron@gmail.com>
2025-09-12 13:11:16 +0900

    MySQL 및 Loki 설치 스크립트 추가: Kubernetes 네임스페이스 생성, Helm을 통한 MySQL 및 Loki 설치, 초기 데이터 로딩 및 Ingress 설정을 포함하여 로그 수집 환경을 구성함.

--- backend/startup.sh
+++ backend/startup.sh
@@ -1,67 +1,16 @@
 #!/bin/bash
 
-SCRIPT_PATH="$(readlink -f "${BASH_SOURCE[0]}")"
-absolute_path="$(dirname "$SCRIPT_PATH")"
-cd "$absolute_path"
-pwd
+cd "$FM_HOME_DIR"
 
-source .env.development
 env | grep -E "^(FM_|WEB_|MYSQL_)" | sort
 
-db_name="fm_db"
-
-pidfile="uvicorn.pid"
+pidfile="backend/uvicorn.pid"
 if [ -f "$pidfile" ]; then
     echo "Killing old process..."
     kill "$(cat $pidfile)" && rm -f "$pidfile"
     sleep 2
 fi
 
-if [ -e nohup.out ]; then
-    mv nohup.out nohup.out.old
-fi
-
-echo "###### create network ######"
-docker network create fm_network > /dev/null 2>&1 || echo "Network 'fm_network' already exists."
-
-if [ "$1" = "-r" ]; then
-    echo "###### remove DB server ######"
-    if docker inspect "$db_name" > /dev/null; then
-        docker rm -f "$FM_DB_HOST"
-        rm -rf $(dirname $(pwd))/mysql_data_dir
-    fi
-fi
-
-if docker inspect -f '{{.State.Running}}' "$db_name" | grep true > /dev/null 2>&1; then
-    echo "###### DB server is already running ######"
-else
-    echo "###### run DB server ######"
-    mkdir -p "$absolute_path/mysql_data_dir" > /dev/null 2>&1
-    chmod 755 "$absolute_path/mysql_data_dir" > /dev/null 2>&1
-
-    docker rm fm_db > /dev/null 2>&1
-    docker pull mysql:8.0.35 > /dev/null
-
-    echo "docker run --name $db_name -d -p $FM_DB_PORT:3306 --network fm_network --user $(id -u):$(id -g) -v $absolute_path/mysql_data_dir:/var/lib/mysql --env-file .env.development mysql:8.0.35"
-    docker run \
-        --name "$db_name" \
-        -d \
-        -p "$FM_DB_PORT":3306 \
-        --network fm_network \
-        --user "$(id -u):$(id -g)" \
-        -v "$absolute_path/mysql_data_dir:/var/lib/mysql" \
-        --env-file .env.development \
-        mysql:8.0.35
-    echo "###### mysql is running ######"
-    sleep 2
-fi
-
-docker ps
-
 echo "###### run backend ######"
-#nohup uvicorn backend.main:app --reload --workers=4 --port="$FM_BACKEND_PORT" &
-#echo "$!" > "$pidfile"
-#sleep 2
-#tail nohup.out
-PYTHONPATH="$(dirname "$absolute_path"):$PYTHONPATH" uvicorn backend.main:app --reload --port="$FM_BACKEND_PORT"
+uvicorn backend.main:app --reload --port="$FM_BACKEND_PORT"
 
