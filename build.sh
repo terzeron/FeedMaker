670d2326f5f322c42f4d15c9ce27ea911ca4b324
Youngil Cho <terzeron@gmail.com>
2025-11-13 09:32:43 +0900

    두 개의 Dockerfile의 위치를 각각 backend와 frontend 하위로 변경하고 build.sh에 반영

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ export USER_NAME=$(whoami)
 # Clean up unnecessary files before build
 rm -rf -- */{nohup.out*,run.log*,.mypy_cache,__pycache__,.idea,.git}
 
-docker build -f Dockerfile.backend --build-arg FM_BACKEND_PORT="$FM_BACKEND_PORT" -t terzeron/fm_backend . && \
+docker build -f backend/Dockerfile --build-arg FM_BACKEND_PORT="$FM_BACKEND_PORT" -t terzeron/fm_backend . && \
 docker tag terzeron/fm_backend:latest registry.terzeron.com/terzeron/fm_backend:latest && \
 docker push registry.terzeron.com/terzeron/fm_backend:latest || exit 1
 
-docker build -f Dockerfile.frontend -t terzeron/fm_frontend . && \
+docker build -f frontend/Dockerfile -t terzeron/fm_frontend . && \
 docker tag terzeron/fm_frontend:latest registry.terzeron.com/terzeron/fm_frontend:latest && \
 docker push registry.terzeron.com/terzeron/fm_frontend:latest || exit 1
 
