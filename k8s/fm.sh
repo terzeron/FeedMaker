064cd8e2869598acfe392eb8acdca682d34350d2
Youngil Cho <terzeron@gmail.com>
2026-03-05 14:56:41 +0900

    feat(k8s): FM 서비스 Kubernetes 배포 설정 추가
    - FM Backend 배포 설정 추가 (Deployment, Service, 환경변수)
    - FM Frontend 배포 설정 추가 (Deployment, Service)
    - TLS 인증서 설정 추가 (cert-manager)
    - PersistentVolume 및 PersistentVolumeClaim 설정
    - Kubernetes 배포 자동화 스크립트 추가

---
+++ k8s/fm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash 
+
+cd ~/k8s
+
+
+kubectl create namespace feedmaker
+
+# volume
+echo "applying fm-volume"
+kubectl apply -f fm-volume.yml
+
+# nginx
+echo "installing nginx by helm"
+helm install web bitnami/nginx -n feedmaker --version 18.3.5 --create-namespace --set staticSitePVC=public-html-pvc --set replicaCount=1 -f web-nginx-values.yml
+
+# deployments
+echo "installing mysql by helm"
+helm install fm-db bitnami/mysql -n feedmaker --create-namespace --set auth.rootPassword=micro170762 --set auth.database=feedmaker --set auth.username=feedmaker --set auth.password=micro170433 --set volumePermissions.enabled=true --set primary.persistence.enabled=true --set primary.persistence.existingClaim=mysql-pvc --set primary.service.type=LoadBalancer
+echo "initializing"
+kubectl exec -i fm-db-mysql-0 -- mysql -u feedmaker feedmaker -pmicro170433 < ~/workspace/fm/init.sql
+
+echo "applying fm-configmap"
+kubectl apply -f fm-configmap.yml
+echo "applying fm-deployment"
+kubectl apply -f fm-deployment.yml
+
+# browserless
+helm install fm skm/browserless-chrome -n feedmaker --create-namespace --set replicaCount=2
+
+# ingress
+echo "applying fm-ingress"
+kubectl apply -f fm-ingress.yml
+
