f6ddc96a5aca809d9564c40c18704b2e4c7338f3
Youngil Cho <terzeron@gmail.com>
2026-04-22 09:58:42 +0900

    fix(k8s): MySQL 헬름 차트 버전 및 프로브 설정 지정

--- k8s/fm.sh
+++ k8s/fm.sh
@@ -15,7 +15,7 @@ helm install web bitnami/nginx -n feedmaker --version 18.3.5 --create-namespace 
 
 # deployments
 echo "installing mysql by helm"
-helm install fm-db bitnami/mysql -n feedmaker --create-namespace --set auth.rootPassword=micro170762 --set auth.database=feedmaker --set auth.username=feedmaker --set auth.password=micro170433 --set volumePermissions.enabled=true --set primary.persistence.enabled=true --set primary.persistence.existingClaim=mysql-pvc --set primary.service.type=LoadBalancer
+helm install fm-db bitnami/mysql -n feedmaker --create-namespace --version 9.19.1 --set auth.rootPassword=micro170762 --set auth.database=feedmaker --set auth.username=feedmaker --set auth.password=micro170433 --set volumePermissions.enabled=true --set primary.persistence.enabled=true --set primary.persistence.existingClaim=mysql-pvc --set primary.service.type=LoadBalancer --set primary.livenessProbe.timeoutSeconds=5 --set primary.terminationGracePeriodSeconds=60
 echo "initializing"
 kubectl exec -i fm-db-mysql-0 -- mysql -u feedmaker feedmaker -pmicro170433 < ~/workspace/fm/init.sql
 
